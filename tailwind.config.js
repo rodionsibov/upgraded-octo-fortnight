c154164955ede933bbde8601e96987518f7932d8
Rodion Sibov <rodionsibov@gmail.com>
2021-06-18 12:58:57 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
 module.exports = {
   purge: {
-    enabled: false,
+    enabled: true,
     content: ['./public/**/*.html']
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      inset: {
+        '17': '68px',
+        '26': '104px'
+      }
+    },
   },
   variants: {
     extend: {},
